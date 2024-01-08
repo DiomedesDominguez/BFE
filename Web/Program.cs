d23aea657f0fc45e0fc02aec8a168635afbca67e
Diomedes Ignacio Domínguez Ureña <diomedes.dominguez@gmail.com>
2024-01-08 09:10:18 -0400

    Update Program.cs
    Swagger me hace falta

--- Web/Program.cs
+++ Web/Program.cs
@@ -12,12 +12,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
-app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
